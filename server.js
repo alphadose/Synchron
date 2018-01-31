66218d0ee93ffe0b60cf4b75cf53bf0330f3cc46
Nikhil <nikhil.celtics@gmail.com>
2018-02-01 01:06:23 +0530

    added views layout and basic home page

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 var express = require('express');
 var app = express();
+var ejsLayouts = require("express-ejs-layouts");
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
+app.use(ejsLayouts);
 var config = require('./config/config.json');
 
 app.listen(config.port);
 
 app.get('/', function(req, res) {
-	res.send("listening");
+	res.render("home")
 });
