7a3c5f311c49918aca0045ea719b3ddbc6f9f572
Nikhil <nikhil.celtics@gmail.com>
2018-02-01 21:49:34 +0530

    added room class

--- server.js
+++ server.js
@@ -1,23 +1,40 @@
 var express = require('express');
 var app = express();
 var ejsLayouts = require("express-ejs-layouts");
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var Room = require('./room.js');
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
 app.use(ejsLayouts);
 var config = require('./config/config.json');
+app.set('port', config.port||3000);
+http.listen(app.get('port'), function() {
+	console.log("http server listening...");
+});
 
-app.listen(config.port);
-
+var cluster = {};
 app.get('/', function(req, res) {
-	res.render("home");
+	res.render('home');
 });
 
 app.get('/create', function(req, res) {
-	res.render("create");
+	res.render('room-admin');
+	io.on('connection', function(socket) {
+		socket.on('peerId', function(id) {
+			var room = new Room(id);
+		});
+	});
 });
 
-app.get('/join', function(req,  res) {
-	res.render("join");
+app.get('/room/:id', function(req,  res) {
+	res.render("room");
 });
+
+io.on('connection', function(socket) {
+	socket.on('peerId', function(id) {
+
+	});
+})
