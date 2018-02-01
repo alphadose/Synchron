32a10403dcfabc367952a303944bd3f03aa63ecb
Nikhil <nikhil.celtics@gmail.com>
2018-02-02 01:29:29 +0530

    fixed copied url

--- public/js/room-admin.js
+++ public/js/room-admin.js
@@ -7,15 +7,15 @@ var peer = new Peer({key : '2pr6j8fsr9roogvi'});
 peer.on('open', function(id) {
 	socket.emit('peerId', id);
 })
-socket.on('sendRoomId', function(id) {
+socket.on('sendUrl', function(url) {
 	swal({
 			title: "Room has been created",
-			text: "ID : " + id,
+			text: "Share the url with your friends!",
 			icon: "success",
 			button: "COPY URL"
 	})
 	.then (() => {
-		$("#copy-btn").attr("data-clipboard-text", socket.id);
+		$("#copy-btn").attr("data-clipboard-text", url);
 		$("#copy-btn").trigger("click");
 	});
 })
