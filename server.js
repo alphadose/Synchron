9e68d66182b3bf1d9c6592eb4b4d4d207d0504c6
Alphadose <anish.mukherjee1996@gmail.com>
2018-02-02 07:19:34 +0530

    Added synchronous play feature

--- server.js
+++ server.js
@@ -1,28 +1,23 @@
 var express = require('express');
 var app = express();
-<<<<<<< HEAD
 var ejsLayouts = require("express-ejs-layouts");
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var Room = require('./room.js');
-=======
-var bodyParser = require('body-parser');
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
-const util = require('util');
->>>>>>> 46c3803... Basic streaming feature added
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
 app.use(ejsLayouts);
 var config = require('./config/config.json');
+
 app.set('port', config.port||3000);
 http.listen(app.get('port'), function() {
   console.log('Http magic is happening on port ' + app.get('port'));
 });
 
 var cluster = {};
+
 app.get('/', function(req, res) {
 	res.render('home');
 });
@@ -67,4 +62,24 @@ io.on('connection', function(socket) {
 			});
 		}
 	});
+
+   socket.on('disconnect', function(){
+    console.log('user disconnected');
+    connectCounter--;
+  });
+
+  socket.on('function', function(action, roomId){
+    io.in(cluster[roomId]).emit('execute', action);
+  });
+
+  socket.on('clear', function(roomId){
+    cluster[roomId].load = 0;
+  });
+
+  socket.on('standby', function(roomId){
+    cluster[roomId].load++;
+    if( cluster[roomId].load === cluster[roomId].strength)
+      io.in(cluster[roomId]).emit('go');
+  });
+
 });
