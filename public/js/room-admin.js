1a527b8c77a8b0334f77ce3a36b353428543a7ed
Alphadose <anish.mukherjee1996@gmail.com>
2018-02-07 19:54:36 +0530

    removed sweetalert

--- public/js/room-admin.js
+++ public/js/room-admin.js
@@ -2,7 +2,7 @@ var clipboard = new Clipboard('.btn');
 var roomId;
 
 socket.on('sendUrl', function(url) {
-	swal({
+/*	swal({
 			title: "Room has been created",
 			text: "Share the url with your friends!",
 			icon: "success",
@@ -12,7 +12,7 @@ socket.on('sendUrl', function(url) {
 		$("#copy-btn").attr("data-clipboard-text", url);
 		$("#copy-btn").trigger("click");
 	});
-});
+*/});
 
 socket.on('store', function(id){
 
