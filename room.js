96d098be22c1bdba35341968d9e3cc966ef122ef
Alphadose <anish.mukherjee1996@gmail.com>
2018-03-08 01:09:22 +0530

    Revamped for local testing

--- room.js
+++ room.js
@@ -7,6 +7,10 @@ function Room(socketId, peerId, username) {
 	this.load = 0;
 	this.adminHandle = username;
 	this.roomHandle = username.toUpperCase() + "'s ROOM";
+	this.positions = [[-2,0,1],
+					  [0,0,1],
+					  [2,0,1]];
+	this.index = 2;
 }
 
 Room.prototype.addMember = function(id, username) {
