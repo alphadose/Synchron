7fe0bea7558e47366b62d5351b73ac759c01dc29
Nikhil <nikhil.celtics@gmail.com>
2018-06-05 13:12:08 +0530

    minor fix

--- public/js/room-admin.js
+++ public/js/room-admin.js
@@ -2,11 +2,7 @@ var clipboard = new Clipboard('.btn');
 clipboard.on('success', function(e) {
 	
 })
-var socket = io();
-var peer = new Peer({key : '2pr6j8fsr9roogvi'});
-peer.on('open', function(id) {
-	socket.emit('peerId', id);
-})
+
 socket.on('sendUrl', function(url) {
 	swal({
 			title: "Room has been created",
