a2685c08f87286ec154c2338179ab9042e2ae4dd
harsh_jain <harshjniitr@gmail.com>
2018-06-12 09:29:47 +0530

    add 3d room

--- public/js/player.js
+++ public/js/player.js
@@ -3,17 +3,47 @@ var context;
 var subtitles;
 var bufferLoader;
 var queue = [];
-var source;
 var playing = 0;
 var susresBtn = document.getElementById("pause");
 var timer;
 var originalTime;
 var pauseTime = 0;
 
+let noOfSpeaker = 2;
+
+var audioElements = [];
+let soundSources = [];
+let audioReady = false;
+let exec = false;
+let position = null;
+let person = null;
+let bufferListSongs = {};
+let songbird = null;
+
+let speakerPos = {
+  "0": { x: -3, y: 2, z: -3 },
+  "1": { x: 3, y: 2, z: -3 },
+}
+
 function init() {
   // Fix up prefixing
   window.AudioContext = window.AudioContext || window.webkitAudioContext;
   context = new AudioContext();
+  songbird = new Songbird(context);
+  var dimensions = {
+    width: 3.1,
+    height: 2.5,
+    depth: 3.4
+  };
+  var materials = {
+    left: 'curtain-heavy',
+    right: 'curtain-heavy',
+    front: 'curtain-heavy',
+    back: 'curtain-heavy',
+    down: 'grass',
+    up: 'transparent'
+  };
+  songbird.setRoomProperties(dimensions, materials);
 }
 
 async function fetch() {
@@ -22,30 +52,30 @@ async function fetch() {
     return toast("Already Playing");
 
   if (queue.length === 0)
-  	return toast("Queue is empty");
+    return toast("Queue is empty");
 
   bufferLoader = await new BufferLoader(
     context,
     [queue[0]],
     synchronise
-    );
+  );
 
   if (typeof timer !== 'undefined')
     timer.clear();
 
-  await loadJSON(function(response) {
+  await loadJSON(function (response) {
     subtitles = JSON.parse(response);
   }, '/audio/example.json');
 
-  queue.splice(0,1);
+  queue.splice(0, 1);
   bufferLoader.load();
 
 }
 
-async function add(url='/audio/example.mp3') {
+async function add(url = '/audio/example.mp3') {
 
-	await queue.push(url);
-	return toast("Added");
+  await queue.push(url);
+  return toast("Added");
 
 }
 
@@ -53,12 +83,13 @@ async function next() {
 
   socket.emit('clear', roomId);
 
-  if (typeof source !== 'undefined')
-    source.onended = null;
+  if (audioElements.length)
+    for (i = 0; i < noOfSpeaker; i++)
+      audioElements[i].onended = null;
 
-  if (playing === 1)
-  { 
-    source.stop();
+  if (playing === 1) {
+    for (i = 0; i < noOfSpeaker; i++)
+      audioElements[i].stop();
     playing = 0;
   }
 
@@ -66,25 +97,49 @@ async function next() {
   await fetch();
 
   if (context.state === 'suspended')
-    context.resume(); 
+    context.resume();
 
 }
 
 async function synchronise(bufferList) {
 
-  source = await context.createBufferSource();
-  source.onended = next;
-  source.buffer = bufferList[0];
+  await songbird.output.connect(context.destination);
 
-  await source.connect(context.destination);
+  bufferListSongs = bufferList;
+  for (i = 0; i < noOfSpeaker; i++) {
+    audioElements[i] = await context.createBufferSource();
+    audioElements[i].buffer = bufferListSongs[0];
+  }
+
+  for (let i = 0; i < noOfSpeaker; i++) {
+    soundSources[i] = songbird.createSource();
+    audioElements[i].connect(soundSources[i].input);
+  }
+  /* setpositionofspeaker */
+  for(i=0; i<noOfSpeaker; i++)
+    soundSources[i].setPosition(speakerPos[i.toString()].x, speakerPos[i.toString()].y, speakerPos[i.toString()].z);
+
+  let gain = context.createGain();
+  gain.gain.value = 1 / (2 * noOfSpeaker);
+  songbird.output.connect(gain);
+  gain.connect(context.destination);
+  console.log("called");
+  console.log(audioElements);
+  for (i = 0; i < noOfSpeaker; i++) {
+    //audioElements[i].start(0);
+  }
+  exec = true;
+
   socket.emit('standby', roomId);
 
 }
 
 async function finishedLoading() {
 
-  if ( playing === 0 )
-    source.start(0);
+  if (playing === 0)
+  for(i=0; i<noOfSpeaker; i++){
+    audioElements[i].start(0);
+  }
 
   timer = await new InvervalTimer(function () {
     displaySubtitles(subtitles);
@@ -96,101 +151,102 @@ async function finishedLoading() {
 
 async function pauseres() {
 
-  if(context.state === 'running') {
-    context.suspend().then(function() {
+  if (context.state === 'running') {
+    context.suspend().then(function () {
       if (typeof timer !== 'undefined')
         timer.pause();
       if (typeof pauseTime !== 'undefined')
-        pauseTime = new Date().getTime()/1000;
+        pauseTime = new Date().getTime() / 1000;
       susresBtn.textContent = 'Resume';
 
     });
 
-  } else if(context.state === 'suspended') {
-    context.resume().then(function() {
+  } else if (context.state === 'suspended') {
+    context.resume().then(function () {
       if (typeof timer !== 'undefined')
         timer.resume();
       if (typeof originalTime !== 'undefined' && typeof pauseTime !== 'undefined')
-        originalTime+= (new Date().getTime()/1000 - pauseTime);
+        originalTime += (new Date().getTime() / 1000 - pauseTime);
       susresBtn.textContent = 'Pause';
 
-    });  
+    });
   }
 }
 
-function toast(message, timeout=2000) {
+function toast(message, timeout = 2000) {
 
   var x = document.getElementById("snackbar")
   x.innerHTML = message;
-        document.getElementById("fixed-snackbar").setAttribute("text", {
-          value: message
-        })
+  document.getElementById("fixed-snackbar").setAttribute("text", {
+    value: message
+  })
 
   x.className = "show";
 
-  setTimeout(function(){ x.className = x.className.replace("show", ""); }, timeout);
-        setTimeout(function(){        
-         document.getElementById("fixed-snackbar").setAttribute("text", {
-          value: ''
-        })}, timeout);
+  setTimeout(function () { x.className = x.className.replace("show", ""); }, timeout);
+  setTimeout(function () {
+    document.getElementById("fixed-snackbar").setAttribute("text", {
+      value: ''
+    })
+  }, timeout);
 }
 
 function InvervalTimer(callback, interval) {
 
   var timerId, startTime, remaining = 0;
-    var state = 0; //  0 = idle, 1 = running, 2 = paused, 3= resumed
-    originalTime = new Date().getTime()/1000;
+  var state = 0; //  0 = idle, 1 = running, 2 = paused, 3= resumed
+  originalTime = new Date().getTime() / 1000;
 
-    this.pause = function () {
-      if (state != 1) return;
+  this.pause = function () {
+    if (state != 1) return;
 
-      remaining = interval - (new Date() - startTime);
-      window.clearInterval(timerId);
-      state = 2;
-    };
+    remaining = interval - (new Date() - startTime);
+    window.clearInterval(timerId);
+    state = 2;
+  };
 
-    this.clear = function () {
-      window.clearInterval(timerId);
-    };
+  this.clear = function () {
+    window.clearInterval(timerId);
+  };
 
-    this.resume = function () {
-      if (state != 2) return;
+  this.resume = function () {
+    if (state != 2) return;
 
-      state = 3;
-      window.setTimeout(this.timeoutCallback, remaining);
-    };
+    state = 3;
+    window.setTimeout(this.timeoutCallback, remaining);
+  };
 
-    this.timeoutCallback = function () {
-      if (state != 3) return;
+  this.timeoutCallback = function () {
+    if (state != 3) return;
 
-      callback();
-
-      startTime = new Date();
-      timerId = window.setInterval(callback, interval);
-      state = 1;
-    };
+    callback();
 
     startTime = new Date();
     timerId = window.setInterval(callback, interval);
     state = 1;
-  }
+  };
 
-  async function displaySubtitles(subs) {
+  startTime = new Date();
+  timerId = window.setInterval(callback, interval);
+  state = 1;
+}
 
-    var t = new Date().getTime()/1000 - originalTime;
-    var n = document.getElementById("subs");
+async function displaySubtitles(subs) {
 
-    console.log(t);
+  var t = new Date().getTime() / 1000 - originalTime;
+  var n = document.getElementById("subs");
 
-    subtitles.forEach(function(element, index, array) {
+  console.log(t);
 
-      if( t >= element.start && t <= element.end ) {
+  subtitles.forEach(function (element, index, array) {
 
-        document.getElementById("subs").setAttribute("text", {
-          value: element.text
-        })
+    if (t >= element.start && t <= element.end) {
 
-      }
+      document.getElementById("subs").setAttribute("text", {
+        value: element.text
+      })
 
-    });
-  }
+    }
+
+  });
+}
