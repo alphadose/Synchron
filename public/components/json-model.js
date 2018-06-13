7208645e0b04763ad6814428d031ffd55e224c35
harsh_jain <harshjniitr@gmail.com>
2018-06-13 13:12:08 +0530

    multi model

--- public/components/json-model.js
+++ public/components/json-model.js
@@ -1,10 +1,11 @@
-let modelMixer, modelClock = new THREE.Clock();
+let modelMixer = [], modelClock = [] ;
 AFRAME.registerComponent('json-model', {
   schema: {
     src: { type: 'string' },
     x: { type: 'number' },
     y: { type: 'number' },
     z: { type: 'number' },
+    anglevalue: { type: 'number' },
   },
   init: function () {
     console.log("model called");
@@ -21,9 +22,16 @@ animate();
         });
         let modelmesh = new THREE.SkinnedMesh( geometry, new THREE.MeshFaceMaterial(materials));
         modelmesh.position.set( data.x, data.y, data.z );
+        if(data.z<=0)
+        modelmesh.rotateY(data.anglevalue);
+        else
+        modelmesh.rotateY(data.anglevalue - 180);
+        let modelclocka= new THREE.Clock();
+        modelClock.push(modelclocka);
         modelScene.add( modelmesh );
-        modelMixer = new THREE.AnimationMixer( modelmesh );
-        modelAction = modelMixer.clipAction( geometry.animations[0] );
+        let modelMixera = new THREE.AnimationMixer( modelmesh );
+        modelAction = modelMixera.clipAction( geometry.animations[0] );
+        modelMixer.push(modelMixera);
         modelAction.play();
       } );
   }
@@ -32,7 +40,8 @@ animate();
 
 function animate() {
   requestAnimationFrame( animate );
-  if( modelMixer ) {
-    modelMixer.update( modelClock.getDelta() );
+  for(i = 0; i< modelMixer.length;i++ )
+  if( modelMixer[i] ) {
+    modelMixer[i].update( modelClock[i].getDelta() );
   }
 }
